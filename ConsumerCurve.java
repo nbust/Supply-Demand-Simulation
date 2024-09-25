72b41dc44156ff76709c568e8cca959fbc5b5f0e
nbust <ncbuster@ecusd7.org>
2024-09-25 09:30:16 -0500

    Create ConsumerCurve.java

---
+++ ConsumerCurve.java
@@ -0,0 +1,19 @@
+package phase2;
+
+/**
+ * @author Noah Buster, Justin Dial, Olivia Sayuk
+ * Date: 2024 Sep 24
+ * Description: ConsumerCurve extends abstract curve
+ * for producer class
+ */
+public class ConsumerCurve extends AbstractCurve {
+	
+	
+	/**
+	 * sorts AbstractCurve from lowest to highest quantity
+	 */
+	@Override
+	public void sort()
+	{
+	}
+}
